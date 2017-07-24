6e096ab724d40b4988c89221e3339e94bca3df20
Mohammed Mahdy <mohamed.mahdy@sphinxinfotech.net>
2017-07-24 11:58:00 +0200

    lesson 13 arrow functions

---
+++ 013.js
@@ -0,0 +1,7 @@
+var employee = function (employee) {
+    console.log(employee);
+};
+var employee2 = function (employee) { return console.log(employee); }; // arrow function
+employee(['Mahdy', 'Rasha', 'Eman']);
+employee2(['Mahdy', 'Rasha', 'Eman']);
+//# sourceMappingURL=013.js.map
---
+++ 013.ts
@@ -0,0 +1,9 @@
+var employee = function(employee){
+    console.log(employee);
+}
+
+
+var employee2 = (employee) => console.log(employee); // arrow function
+
+employee(['Mahdy', 'Rasha', 'Eman']);
+employee2(['Mahdy', 'Rasha', 'Eman']);
